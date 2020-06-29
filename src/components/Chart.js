02db44ae7b67cc3604efd87d0b9f6acd4a09ee7f
Jonah <jonah.freeland@mrbandfriends.co.uk>
2020-06-29 23:11:18 +0100

    format dates

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -11,6 +11,11 @@ import {
 } from "recharts";
 
 function Chart(props) {
+  const formatDate = (label) => {
+    const date = new Date(label);
+    const dateStr = date.toLocaleDateString("en-US");
+    return dateStr.substring(0, dateStr.length - 2);
+  };
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
@@ -19,18 +24,25 @@ function Chart(props) {
       >
         <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
         <CartesianGrid vertical={false} />
-        <XAxis dataKey="date" minTickGap={20}>
-          <Label value="Date" dy={24} position="insideBottom" />
+        <XAxis
+          dataKey="date"
+          minTickGap={20}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(label) => formatDate(label)}
+        >
+          <Label value="Date" dy={32} position="insideBottom" />
         </XAxis>
-        <YAxis dataKey="value">
+        <YAxis dataKey="value" axisLine={false} tickMargin={8}>
           <Label
             value="Trees planted"
-            dx={-20}
+            dx={-28}
             angle={-90}
             position="insideLeft"
           />
         </YAxis>
         <Tooltip
+          labelFormatter={(label) => formatDate(label)}
           formatter={(value, name) => [
             new Intl.NumberFormat("en").format(value),
             "Trees",
